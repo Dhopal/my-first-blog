b0ce426dc3b6da2e7b84e839446ec7a621fdd59e
saurabh <anand100saurabh@gmail.com>
2017-12-15 20:10:46 +0530

    Completed adding Comment feature.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
+
+
+# class CommentAdmin(admin.ModelAdmin):
+#     list_display = ('text', 'post')
+#     list_filter = (
+#         ('post', admin.RelatedOnlyFieldListFilter),
+#     )
 
 admin.site.register(Post)
+# admin.site.register(Comment, CommentAdmin)
+admin.site.register(Comment)
